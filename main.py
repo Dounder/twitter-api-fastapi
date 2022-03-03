130354eb45988b4aaed0d296ec1151a42be0c07f
dcode-otb <frontend@agenciaotb.com>
2022-03-03 16:17:18 -0600

    done: tweets endpoints

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ from typing import List
 from fastapi import FastAPI, status
 # Models
 from models.user import User
+from models.tweet import Tweet
 
 app = FastAPI()
 
 # * Path Operations
-@app.get(path='/', tags=['home'])
-def home():
-    return {'Twitter API': 'Working!'}
-
 # ? Users
 @app.post(
     path='/auth/signup',
@@ -85,3 +82,56 @@ def delete_user():
     pass
 
 # ? Tweets
+@app.get(
+    path='/',
+    response_model=List[Tweet],
+    status_code=status.HTTP_200_OK,
+    summary='Get all tweets',
+    tags=['Tweets']
+)
+def get_tweets():
+    pass
+
+
+@app.get(
+    path='/tweets/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Get a specific tweet',
+    tags=['tweets']
+)
+def get_tweet():
+    pass
+
+
+@app.post(
+    path='/tweet',
+    response_model=Tweet,
+    status_code=status.HTTP_201_CREATED,
+    summary='Create a tweet',
+    tags=['Tweets']
+)
+def create_tweet():
+    pass
+
+
+@app.put(
+    path='/tweet/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Update a tweet',
+    tags=['Tweets']
+)
+def update_tweet():
+    pass
+
+
+@app.delete(
+    path='/tweet/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Delete a tweet',
+    tags=['Tweets']
+)
+def delete_tweet():
+    pass
